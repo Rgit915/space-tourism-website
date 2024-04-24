f961486a89716935e46a96990401f2f24a87e582
Rgit915 <rora.goitom@gmail.com>
2024-04-24 11:14:10 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+const App = () => {
+  return (
+    <>
+      <header>
+        <nav>
+          <div>
+            <img src="" alt="logo" />
+          </div>
+          <div>00 Home 01 Destination 02 Crew 03 Technology</div>
+        </nav>
+      </header>
+      
+      <main>
+        <section>
+          <div>
+            So, you want to travel to Space Let’s face it; if you want to go to
+            space, you might as well genuinely go to outer space and not hover
+            kind of on the edge of it. Well sit back, and relax because we’ll
+            give you a truly out of this world experience!
+          </div>
+          <div>Explore</div>
+        </section>
+      </main>
+
+      <footer></footer>
+    </>
+  );
+};
+
+export default App;
