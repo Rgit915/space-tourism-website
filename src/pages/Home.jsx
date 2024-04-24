64c65e82a4a6ae27c9709ff86f018ad4f36127cf
Rgit915 <rora.goitom@gmail.com>
2024-04-24 16:05:47 -0400

    Add home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <section>
+    <div>
+      So, you want to travel to Space Let’s face it; if you want to go
+      to space, you might as well genuinely go to outer space and not
+      hover kind of on the edge of it. Well sit back, and relax because
+      we’ll give you a truly out of this world experience!
+    </div>
+    <div>Explore</div>
+  </section>
+
+  )
+}
+
+export default Home
