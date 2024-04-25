bc38be0974bc26cefa0799f6be594670c0ec4b07
Rgit915 <rora.goitom@gmail.com>
2024-04-25 08:19:05 -0400

    Extend font colors and define custom font families

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     "./src/**/*.{js,jsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+
+        'dark-blueish': '#0B0D17',
+        'light-blue': '#D0D6F9',
+        'white': '#FFFFFF',
+
+      },
+      fontFamily: {
+        'barlow-condensed': ["Barlow Condensed", "sans-serif"],
+        'barlow': ["Barlow", "sans-serif"],
+        'bellefair': ["Bellefair", "serif"],
+      },
+    },
   },
   plugins: [],
 }
