622e67a4f445406eec893e4ea9c0583decfad2e0
Rgit915 <rora.goitom@gmail.com>
2024-04-25 08:21:39 -0400

    Add flex container for logo

--- src/App.jsx
+++ src/App.jsx
@@ -14,26 +14,26 @@ const App = () => {
     <>
       <BrowserRouter>
         <header>
-          <nav>
+          <nav className="flex justify-between items-center ">
             <div>
               <img src={logo} alt="logo" />
             </div>
             <div>
-              <div>
+              <div className="">
                 <button type="button">
                   <img src={menuIcon} alt="Navigation menu" />
                 </button>
-              </div>
-              <div>
-                <button type="button">
+
+                <button type="button" className="hidden">
                   <img src={closeIcon} alt="close Icon" />
                 </button>
               </div>
-
-              <NavLink to="/"> 00 Home </NavLink>
-              <NavLink to="destination">01 Destination </NavLink>
-              <NavLink to="crew">02 Crew</NavLink>
-              <NavLink to="technology">03 Technology</NavLink>
+              <div className="hidden">
+                <NavLink to="/"> 00 Home </NavLink>
+                <NavLink to="destination">01 Destination </NavLink>
+                <NavLink to="crew">02 Crew</NavLink>
+                <NavLink to="technology">03 Technology</NavLink>
+              </div>
             </div>
           </nav>
         </header>
