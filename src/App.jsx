987c6e8cb90884a521341133adc7018cc850a2d5
Rgit915 <rora.goitom@gmail.com>
2024-04-25 23:40:47 -0400

    Update router with createBrowser function

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
-import Header from "./components/Header";
-import AppRoutes from "./routes/AppRoutes";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
 
+import Home from "./pages/Home";
+import Destination from "./pages/Destination";
+import Crew from "./pages/Crew";
+import Technology from "./pages/Technology";
+
+
+import RootLayout from "./layouts/RootLayout";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout/>}>
+      <Route index element={<Home />} />
+      <Route path="destination" element={<Destination />} />
+      <Route path="crew" element={<Crew />} />
+      <Route path="technology" element={<Technology />} />
+    </Route>
+  )
+);
 const App = () => {
   return (
-    <>
-      <Header />
-      <AppRoutes />
-    </>
+
+      <RouterProvider router={router} />
+
   );
 };
 
