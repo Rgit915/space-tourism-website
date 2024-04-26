9a6c58734ccdb846279f14b441185b55ac824716
Rgit915 <rora.goitom@gmail.com>
2024-04-25 20:42:17 -0400

    Add styles to navigation links

--- src/components/NavLinks.jsx
+++ src/components/NavLinks.jsx
@@ -1,12 +1,14 @@
 import { NavLink } from "react-router-dom";
 
 const NavLinks = () => {
+
   return (
-    <div className="hidden">
-      <NavLink to="/"> 00 Home </NavLink>
-      <NavLink to="destination">01 Destination </NavLink>
-      <NavLink to="crew">02 Crew</NavLink>
-      <NavLink to="technology">03 Technology</NavLink>
+    <div className="flex flex-col justify-center items-center text-[16px] tracking-[2.7px] text-white font-barlow-condensed me-8 ">
+
+      <NavLink to="/"> <span>00</span>Home  </NavLink>
+      <NavLink to="destination"> <span>01</span>Destination </NavLink>
+      <NavLink to="crew"> <span>02 </span>Crew </NavLink>
+      <NavLink to="technology"> <span>03</span>Technology</NavLink>
     </div>
   );
 };
