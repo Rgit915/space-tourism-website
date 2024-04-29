3c8ebe252a5c513bd3f1d533e201a937e7a2918b
Rgit915 <rora.goitom@gmail.com>
2024-04-29 17:59:38 -0400

    Update AppRoutes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,21 +1,35 @@
-import { Routes, Route } from "react-router-dom";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
 
-import Home from "../pages/Home";
-import Destination from "../pages/Destination";
-import Crew from "../pages/Crew";
-import Technology from "../pages/Technology";
+import RootLayout from '../layouts/RootLayout'; //Importing layout component
+import Home from '../pages/Home';
+import Destination from '../pages/Destination';
+import Crew from '../pages/Crew';
+import Technology from '../pages/Technology';
 
-const AppRoutes = () => {
-  return (
-    <main>
-      <Routes>
+// Creating a browser router instance using createBrowserRouter
+const router = createBrowserRouter(
+  // Creating routes from JSX elements using createRoutesFromElements
+    createRoutesFromElements(
+      <Route path="/" element={<RootLayout/>}>
         <Route index element={<Home />} />
         <Route path="destination" element={<Destination />} />
         <Route path="crew" element={<Crew />} />
         <Route path="technology" element={<Technology />} />
-      </Routes>
-    </main>
+      </Route>
+    )
   );
+
+const AppRoutes = () => {
+  return(
+    <RouterProvider router={router} />
+  )
+
 };
 
 export default AppRoutes;
+
