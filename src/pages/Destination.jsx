99f7144fd825788793a4e44f85b2757e0dd23c34
Rgit915 <rora.goitom@gmail.com>
2024-04-30 14:10:11 -0400

    Add dynamic data display based on user selection

--- src/pages/Destination.jsx
+++ src/pages/Destination.jsx
@@ -1,33 +1,51 @@
 import PlanetNavLinks from "../components/PlanetNavLinks";
-import moonImage from "../assets/destination/image-moon.png"
+import destinationData from "../data.json";
+import { useState } from "react";
+import DestinationListItem from "../components/DestinationListItem";
 
 const Destination = () => {
+  const planets = destinationData.destinations;
+
+  const [planetData, setPlanetData] = useState({
+    name: planets[0].name,
+    image: planets[0].images.png,
+    description: planets[0].description,
+    distance: planets[0].distance,
+    travel: planets[0].travel,
+  });
+
+  const handlePlanetClick = (index) => {
+    // Update planetData state when a planet link is clicked
+    const selectedPlanet = planets[index];
+    setPlanetData({
+      name: selectedPlanet.name,
+      image: selectedPlanet.images.png,
+      description: selectedPlanet.description,
+      distance: selectedPlanet.distance,
+      travel: selectedPlanet.travel,
+    });
+  };
+
   return (
     <section className="planet-container flex flex-col items-center justify-center mt-8 space-y-4">
-      <p className="navlink font-barlow-condensed  tracking-[2.7px] uppercase "> <span className="opacity-20 font-bold mr-3"> 01 </span> Pick your destination</p>
-      <img src={moonImage} className="w-[170px] h-[170px] "/>
-
-      <nav className="planet-sublinks">
-        <PlanetNavLinks />
-      </nav>
-      <h2 className="planet-name text-[56px] font-bellefair uppercase">Moon</h2>
-
-      <p className="description text-[15px] font-barlow text-light-blue leading-6">
-        See our planet as you’ve never seen it before. A perfect relaxing trip
-        away to help regain perspective and come back refreshed. While you’re
-        there, take in some history by visiting the Luna 2 and Apollo 11 landing
-        sites.
+      {/* Render the planet data based on the selected planet */}
+      <p className="font-barlow-condensed  tracking-[2.7px] uppercase ">
+        <span className="opacity-20 font-bold mr-3"> 01 </span> Pick your
+        destination
       </p>
-      <hr className="w-[327px] bg-[#383B4B]"/>
-      <div className="text-light-blue uppercase flex flex-col py-4">
-        <p className="distance text-[14px] font-barlow-condensed tracking-[2.36px]">
-        Avg. distance </p><span className="text-[28px] font-bellefair ">384,400 km</span>
+      <img
+        src={planetData.image}
+        className="w-[170px] h-[170px] "
+        alt={planetData.name}
+      />
 
-      <p className="travel text-[14px] font-barlow-condensed tracking-[2.36px] mt-8">
-        Est. travel time</p><span className="text-[28px] font-bellefair">3 days</span>
+      {/* Render PlanetNavLinks passing planets array and handlePlanetClick function */}
+      <nav className="planet-sublinks">
+        <PlanetNavLinks planets={planets} onPlanetClick={handlePlanetClick} />
+      </nav>
 
-      </div>
-
+      {/* Render DestinationListItem passing planetData */}
+      <DestinationListItem planetData={planetData} />
     </section>
   );
 };
