04ab6bb23760bb4c11fcd0ed23e1616d199a55ef
Rgit915 <rora.goitom@gmail.com>
2024-05-08 14:48:17 -0400

    Implement route mappings for responsive background classes

--- src/helpers/backgroundUtils.js
+++ src/helpers/backgroundUtils.js
@@ -1,6 +1,6 @@
 //  Utility function to determine the background class based on the current route and screen width.
 
-const getBackgroundClass = () => {
+const getBackgroundClass = (path, screenWidth) => {
 
   // Define mappings of routes to background classes for different screen sizes
    const mappings = {
@@ -27,7 +27,19 @@ const getBackgroundClass = () => {
 
   };
 
+ // Get the mappings for the current route or default to an empty object
+  const routeMappings = mappings[path] || {};
+  let backgroundClass = '';
 
+  if (screenWidth < 768) {
+    backgroundClass = routeMappings.mobile || '';
+  } else if (screenWidth >= 768 && screenWidth < 1024) {
+    backgroundClass = routeMappings.tablet || '';
+  } else {
+    backgroundClass = routeMappings.desktop || '';
+  }
+
+  return backgroundClass;
 };
 
 export default getBackgroundClass;
