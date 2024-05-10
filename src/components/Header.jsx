510e43e5db9ba3286d5f14afed2c72ac94a76650
Rgit915 <rora.goitom@gmail.com>
2024-05-10 01:05:23 -0400

    Add NavLink for logo and horizontal line

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
+import { NavLink } from "react-router-dom";
 
 import logo from "../assets/shared/logo.svg";
 import menuIcon from "../assets/shared/icon-hamburger.svg";
 import closeIcon from "../assets/shared/icon-close.svg";
 import NavLinks from "../components/NavLinks";
+
 
 const Header = () => {
   const [isOpen, setOpen] = useState(false);
@@ -14,25 +16,26 @@ const Header = () => {
 
   return (
     <>
-      <header>
+      <header className="header px-5 flex items-center justify-between bg-black">
         <nav className="flex justify-between items-center ">
-          <div>
+        <div className="py-5">
+          <NavLink to="/">
             <img src={logo} alt="logo" />
+          </NavLink>
+        </div>
+          <div className="horizontal-line hidden absolute bg-white w-[473px] h-[1px] lg:opacity-25 lg:left-[10%] ">
+            <hr />
           </div>
-          <div className="hidden md:flex z-10">
+          <div className="hidden md:flex">
             <NavLinks />
           </div>
           <div className="md:hidden">
             <button type="button" onClick={toggleMenu}>
-              {isOpen ? (
-                <img
-                  src={closeIcon}
-                  className="absolute top-10 right-12 z-10 "
-                  alt="close Icon"
-                />
-              ) : (
-                <img src={menuIcon} alt="Navigation menu" />
-              )}
+              <img
+                src={isOpen ? closeIcon : menuIcon}
+                className="absolute top-10 right-12 z-10 "
+                alt={isOpen ? "Close Icon" : "Menu Icon"}
+              />
             </button>
           </div>
         </nav>
