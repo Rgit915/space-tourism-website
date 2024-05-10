76eedca65219cf6d6c1769c22728d699a72cfc1b
Rgit915 <rora.goitom@gmail.com>
2024-05-09 20:01:09 -0400

    Fix useLocation hook and add dynamic background handling

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,14 +1,29 @@
-import Header from '../components/Header';
-import { Outlet } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import { useLocation, Outlet } from "react-router-dom";
+import getBackgroundClass from "../helpers/backgroundUtils";
+
+import Header from "../components/Header";
 
 const RootLayout = () => {
+  const location = useLocation();
+
+  //state to hold the background class
+  const [backgroundClass, setBackgroundClass] = useState("");
+
+  useEffect(() => {
+    const newBackgroundClass = getBackgroundClass(
+      location.pathname,
+      window.innerWidth
+    );
+    setBackgroundClass(newBackgroundClass);  //update backgroundClass state
+  }, [location.pathname]);  
 
   return (
     <>
       {/* Header component */}
       <Header />
       {/* Main content area with dynamic background class */}
-      <main className={`${getBackgroundClass()}`}>
+      <main className={`m-h-screen ${backgroundClass}`}>
         {/* Outlet for rendering nested route components */}
         <Outlet />
       </main>
