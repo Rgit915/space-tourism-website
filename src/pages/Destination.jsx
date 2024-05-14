fb74a77b6906be5311c8afa4fae9b0aa79056108
Rgit915 <rora.goitom@gmail.com>
2024-05-13 22:34:59 -0400

    Refactor Destination component and Implement activeIndex state

--- src/pages/Destination.jsx
+++ src/pages/Destination.jsx
@@ -2,31 +2,20 @@ import PlanetNavLinks from "../components/PlanetNavLinks";
 import { useState } from "react";
 import DestinationListItem from "../components/DestinationListItem";
 
-const Destination = ({data}) => {
+const Destination = ({ data }) => {
   const planets = data.destinations;
 
-  const [planetData, setPlanetData] = useState({
-    name: planets[0].name,
-    image: planets[0].images.png,
-    description: planets[0].description,
-    distance: planets[0].distance,
-    travel: planets[0].travel,
-  });
+  const [activeIndex, setActiveIndex] = useState(0); // Initialize activeIndex with 0
 
   const handlePlanetClick = (index) => {
-    // Update planetData state when a planet link is clicked
-    const selectedPlanet = planets[index];
-    setPlanetData({
-      name: selectedPlanet.name,
-      image: selectedPlanet.images.png,
-      description: selectedPlanet.description,
-      distance: selectedPlanet.distance,
-      travel: selectedPlanet.travel,
-    });
+    // Update activeIndex when a planet link is clicked
+    setActiveIndex(index);
   };
 
+  const planetData = planets[activeIndex]; // Get planet data based on activeIndex
+
   return (
-    <section className="planet-container flex flex-col items-center justify-center mt-8 space-y-4 ">
+    <section className="planet-container flex flex-col items-center justify-center min-h-screen space-y-4 ">
       {/* Render the planet data based on the selected planet */}
       <p className="title mt-6">
         <span className="opacity-20 font-bold mr-3 md:text-[20px] md:tracking-[3.38px] lg:text-[28px] lg:tracking-[4.72px]">
@@ -37,17 +26,18 @@ const Destination = ({data}) => {
       <div className="lg:flex lg:flex-row lg:justify-between lg:items-center">
         <div className="planet-img bg-white lg:mr-12">
           <img
-            src={planetData.image}
+            src={planetData.images.png}
             className="w-[170px] h-[170px] md:w-[300px] md:h-[300px] lg:w-[445px] lg:h-[445px] "
             alt={planetData.name}
           />
         </div>
 
         <div className="destination-description lg:w-[445px] lg:h-[472px] lg:mt-8">
-          {/* Render PlanetNavLinks passing planets array and handlePlanetClick function */}
+          {/* Render PlanetNavLinks passing planets array, activeIndex, and handlePlanetClick function */}
           <nav className="planet-sublinks">
             <PlanetNavLinks
               planets={planets}
+              activeIndex={activeIndex}
               onPlanetClick={handlePlanetClick}
             />
           </nav>
